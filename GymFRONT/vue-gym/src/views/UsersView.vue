20964debd8fb4f3a180fdc64c9f3d05015c5b7d8
gaabsito <157628366+gaabsito@users.noreply.github.com>
2025-02-06 17:23:04 +0100

    deploy

---
+++ GymFRONT/vue-gym/src/views/UsersView.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { useUsersStore } from '@/stores/users';
+
+
+
+    const store = useUsersStore()
+
+
+    store.findAll()
+
+
+
+
+</script>
+
+<template>
+
+    Listado de usuarios
+    <div v-for="user in store.users">
+        {{ user.name }} {{ user.surname }}
+    </div>
+</template>
